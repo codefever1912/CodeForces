eab56a206d093fe88f53fb03a1478b9ac0d65cc5
codefever1912 <dnyaneshkavate@gmail.com>
2025-06-10 22:47:54 +0530

    Wrote a script to rename files from problem indexes to problem names

--- file_rename.py
+++ file_rename.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from bs4 import BeautifulSoup
 
 def rename_files(directory):
     url = "https://codeforces.com/api/problemset.problems"
@@ -11,17 +10,39 @@ def rename_files(directory):
         return
     
     data = response.json()
-    if data.status != "OK":
+    if data["status"] != "OK":
         print("there was a problem with retrieving the info, please try again")
     else:
         data = data["result"]["problems"]
         problem_map = {
-            f"{str(p["contestID"]) + p["index"]}" : f"{p["name"]}" for p in data
+            f"{str(p["contestId"]) + p["index"]}" : f"{p["name"]}" for p in data
         }
-
 
         for file in os.listdir(directory):
             if file.endswith(('.cpp', '.bin', '.exe')):
-                parts = file.split('.')
+                parts = file.split  ('.')
                 file_name = parts[0]
+                extension = parts[1]
                 
+                if file_name not in problem_map.keys():
+                    print(f"Skipping {file} cuz data for this file could not be retrieved from API")
+                    continue 
+
+                new_file_name = file_name + problem_map[file_name]
+                sanitized_name = new_file_name.replace(' ', '_').replace('/', '_').replace(":", '_')
+
+                old_path = os.path.join(directory, file)
+                new_path = os.path.join(directory, f"{sanitized_name}.{extension}")
+
+                if os.path.exists(new_path):
+                    print(f"Resultant new file already exists, hence skipping renaming {file}")
+                
+                try:
+                    os.rename(old_path, new_path)
+                    print(f"Renamed {file} from {old_path} -> {new_path}")
+                except OSError as e:
+                    print(f"Error in renaming {file} : {e}")
+
+if __name__ == "__main__":
+    directory = './'
+    rename_files(directory)
