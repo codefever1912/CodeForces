84c52c78ea6c31023abd935d4cfeca31a7e1b262
codefever1912 <dnyaneshkavate@gmail.com>
2025-08-02 19:26:38 +0530

    [Round 1040 - Div. 2] Upsolved

--- A_Submission_is_All_You_Need.cpp
+++ A_Submission_is_All_You_Need.cpp
@@ -11,47 +11,17 @@ using namespace std;
 #define vin(v) for(auto &e : v) {cin >> e;}
 
 void solve() {
+    /*
+    mex -> 0 - 1, else sum(t)
+    */
     int n; cin >> n;
     vector<int>v(n);
     vin(v);
 
+    int zero_cnt = count(v.begin(), v.end(), 0);
+    int sum = accumulate(v.begin(), v.end(), 0);
 
-    sort(v.begin(), v.end());
-    vector<int>curr; //Store curr elements that are consecutive
-    curr.push_back(v[0]);
-    int score = 0;
-    for (int i = 1; i < n; i++) {
-        if (v[i] - v[i-1] == 1) {
-            curr.push_back(v[i]);
-        } else {
-            if (curr.size() == 1) {
-                if (curr[0] == 0) score ++;
-                else score += curr[0];
-            } else if (curr.size() > 1) {
-                int subset_sum = accumulate(curr.begin(), curr.end(), 0);
-                int mex = ((curr[0] == 0) ? curr.back() + 1 : 0);
-                score += max(subset_sum, mex);
-            }
-
-            curr.clear();
-            curr.push_back(v[i]);
-        }
-    }
-
-    if (!curr.empty()) {
-        if (curr.size() == 1) {
-            // if (curr[0] == 0 || curr[0] == 1) score += curr[0] + 1;
-            if (curr[0] == 0) score++;
-            else score += curr[0];
-        } else {
-            int sum = accumulate(curr.begin(), curr.end(), 0);
-            int mex = ((curr[0] == 0) ? curr.back() + 1 : 0);
-            score += max(sum, mex);
-        }
-    }
-
-    cout << score << nl;
-
+    cout << sum + zero_cnt << nl;
 }
 
 int main () {
