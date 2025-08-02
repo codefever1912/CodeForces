183dc4841b021f7859f95fab1bd42b1228859583
codefever1912 <dnyaneshkavate@gmail.com>
2025-08-02 19:45:12 +0530

    Moved the CSES directory to be its own

--- CSES/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    char a = 'M',b='L';
-    bool x = a<b;
-    cout << x << endl;
-}
--- CSES/template.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef int long long ll
-#define p1(a) cout << a << endl;
-#define p2(a,b) cout << a << b << endl;
-#define for(i,n) for(int i=0)
-#define readV(a) for(auto &e:a){cin >> e;}
-#define pVec(a) for(auto &e:a){cout << e << " ";}
