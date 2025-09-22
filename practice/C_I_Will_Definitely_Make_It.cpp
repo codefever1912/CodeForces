91aeaf948d8c04b7422689e77204e03b7b37eb01
codefever1912 <dnyaneshkavate@gmail.com>
2025-09-22 10:52:28 +0530

    Upsolved: Round 1037 Div. 3
    Forgot to commit C problem after upsolving huihui :p

--- practice/C_I_Will_Definitely_Make_It.cpp
+++ practice/C_I_Will_Definitely_Make_It.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ll long long
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define nl '\n'
-#define vin(v) for(auto &e : v) {cin >> e;}
+#define vin(v) for (auto &e : v) {cin >> e;}
 
 void solve() {
     int n, k; cin >> n >> k;
@@ -16,25 +16,31 @@ void solve() {
     vin(v);
 
     int mx = *max_element(v.begin(), v.end());
-    vector<int>larger;
+    vector<int> larger;
     for (int x : v) {
-       if (x >= v[k - 1]) larger.push_back(x);
+        if (x >= v[k - 1]) {
+          larger.push_back(x);
+        }
     }
 
     sort(larger.begin(), larger.end());
     int water = 0;
     for (int i = 0; i < larger.size() - 1; i++) {
-        int diff = larger[i+1] - larger[i];
-        if (diff > (larger[i] - water)) {cout << "NO" << nl; return;}
+        int diff = larger[i + 1] - larger[i];
+        if (diff > (larger[i] - water)) {
+            cout << "NO" << nl;
+            return;
+        }
         water += diff;
     }
-    
+
     cout << "YES" << nl;
 }
 
-int main () {
+int main() {
     fastio();
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) {
         solve();
     }
